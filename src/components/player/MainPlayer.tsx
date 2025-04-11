1b63b35a936f068b52bd2bbd63f73b6954d3c2fe
MatMB115 <MatMB115@users.noreply.github.com>
2025-04-11 18:02:15 -0300

    feat: Main player using youtube api

---
+++ src/components/player/MainPlayer.tsx
@@ -0,0 +1,51 @@
+import ReactPlayer from "react-player/youtube";
+import { playlist } from "@/constants/videos";
+import { useRef } from "react";
+
+interface MainPlayerProps {
+    currentVideoIndex: number;
+    startTimeInVideo: number;
+    volume: number;
+    currentColor: string;
+}
+
+export const MainPlayer = ({
+    currentVideoIndex,
+    startTimeInVideo,
+    volume,
+    currentColor,
+}: MainPlayerProps) => {
+    const playerRef = useRef<ReactPlayer>(null);
+
+    return (
+        <div className="relative z-10 flex items-center justify-center h-full px-4">
+            <div
+                className={`w-full max-w-5xl aspect-video rounded-2xl overflow-hidden border-4`}
+                style={{
+                    borderColor: currentColor,
+                    boxShadow: `0 0 15px 4px ${currentColor}, 0 0 30px 8px ${currentColor}33`,
+                }}
+            >
+                <div className="w-full h-[102%] md:h-[100.5%]">
+                    <ReactPlayer
+                        ref={playerRef}
+                        url={playlist[currentVideoIndex].url}
+                        playing
+                        volume={volume}
+                        controls={false}
+                        loop
+                        width="100%"
+                        height="100%"
+                        config={{
+                            playerVars: {
+                                start: startTimeInVideo,
+                                modestbranding: 1,
+                                rel: 0,
+                            },
+                        }}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}; 
