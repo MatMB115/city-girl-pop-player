8f2186a463c2243d1935694fe68442e1281ec2d2
MatMB115 <MatMB115@users.noreply.github.com>
2025-04-11 18:00:41 -0300

    feat: add video playlist constants

---
+++ src/constants/videos.ts
@@ -0,0 +1,45 @@
+export const playlist = [
+    {
+        url: "https://www.youtube.com/watch?v=g9D5ECoAl_8",
+        duration: 3640,
+        themeColor: "#ff00cc", // pink neon
+        title: "City Pop Girl Mix - Groovy Drive"
+    },
+
+    {
+        url: "https://www.youtube.com/watch?v=AAJpz0iEkEI",
+        duration: 3900,
+        themeColor: "#a066ff", // purple neon
+        title: "City Pop Girl Mix - Cosmos Patrol"
+    },
+    {
+        url: "https://www.youtube.com/watch?v=8BbAlA1Tk5M",
+        duration: 3690,
+        themeColor: "#0099ff", // light blue neon
+        title: "City Pop Girl Mix - Love Diary"
+    },
+    {
+        url: "https://www.youtube.com/watch?v=ewx1aoA4FwQ",
+        duration: 3550,
+        themeColor: "#00ffff", // cyan neon
+        title: "City Pop Girl Mix - Endless Spring"
+    },
+    {
+        url: "https://www.youtube.com/watch?v=pCL9b9DEXTw",
+        duration: 3720,
+        themeColor: "#ff6ec7", // hot pink
+        title: "City Pop Girl Mix - Cocktail"
+    },
+    {
+        url: "https://www.youtube.com/watch?v=Uw3FzfYZp3k",
+        duration: 3510,
+        themeColor: "#39ff14", // lime green neon
+        title: "City Pop Girl Mix - Coach : A Love Game"
+    },
+    {
+        url: "https://www.youtube.com/watch?v=0DjXKIXH7OE",
+        duration: 3780,
+        themeColor: "#ffaa00", // orange neon
+        title: "City Pop Girl Mix - Secret of Summer"
+    },
+];
