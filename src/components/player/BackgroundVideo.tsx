2955d06060c82c18c385f89acbbe8b637e6ff1b5
MatMB115 <MatMB115@users.noreply.github.com>
2025-04-11 18:00:54 -0300

    feat: add BackgroundVideo component

---
+++ src/components/player/BackgroundVideo.tsx
@@ -0,0 +1,23 @@
+import ReactPlayer from "react-player/youtube";
+import { playlist } from "@/constants/videos";
+
+interface BackgroundVideoProps {
+    currentVideoIndex: number;
+    startTimeInVideo: number;
+}
+
+export const BackgroundVideo = ({ currentVideoIndex, startTimeInVideo }: BackgroundVideoProps) => {
+    return (
+        <div className="absolute inset-0 z-0 scale-[3] sm:scale-[2] md:scale-[1.2] filter blur-[2px] md:blur-xs opacity-60">
+            <ReactPlayer
+                url={playlist[currentVideoIndex].url}
+                playing
+                muted
+                loop
+                width="100%"
+                height="100%"
+                config={{ playerVars: { start: startTimeInVideo } }}
+            />
+        </div>
+    );
+}; 
